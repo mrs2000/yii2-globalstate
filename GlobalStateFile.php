80af2e8e14432f1b655ad8d91affa8746b0e7823
Melnikov Ruslan <mrs2000@inbox.ru>
2014-12-09 21:37:19 +0800

    first

---
+++ GlobalStateFile.php
@@ -0,0 +1,55 @@
+<?
+namespace mrssoft\globalstate;
+
+use yii\base\Component;
+
+class GlobalStateFile extends Component implements GlobalStateInterface
+{
+    public $filename = 'globalstate.bin';
+
+    public $path = '@runtime';
+
+    private $_data = null;
+
+    public function get($name, $default = null)
+    {
+        $this->read();
+        if (array_key_exists($name, $this->_data))
+        {
+            return $this->_data[$name];
+        }
+        return $default;
+    }
+
+    public function set($name, $value)
+    {
+        $this->_data[$name] = $value;
+        return $this->write();
+    }
+
+    private function read()
+    {
+        if ($this->_data === null) {
+            $path = $this->getPath();
+            $this->_data = [];
+            if (is_file($path) && ($this->_data = file_get_contents($path)))
+            {
+                $this->_data = unserialize($this->_data);
+            }
+        }
+    }
+
+    private function write()
+    {
+        if (empty($this->_data))
+        {
+            $this->_data = [];
+        }
+        return (bool)file_put_contents($this->getPath(), serialize($this->_data));
+    }
+
+    private function getPath()
+    {
+        return \Yii::getAlias($this->path).DIRECTORY_SEPARATOR.$this->filename;
+    }
+}
