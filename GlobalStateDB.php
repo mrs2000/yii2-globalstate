3b7fc138dcbc63927acf342fd23a1ccd50bb247f
Melnikov Ruslan <melnikov@novator.angara.ru>
2015-07-15 09:43:29 +0800

    Fix read/write data in DB variant

--- GlobalStateDB.php
+++ GlobalStateDB.php
@@ -22,13 +22,17 @@ class GlobalStateDB extends Component implements GlobalStateInterface
             return $this->_data;
         }
         if (array_key_exists($name, $this->_data)) {
-            return $this->_data[$name];
+            return $this->getValue($name);
         }
         return $default;
     }
 
     public function set($name, $value = null)
     {
+        if (!is_string($value)) {
+            $value = serialize($value);
+        }
+
         if (is_array($name)) {
             foreach ($name as $key => $value) {
                 $this->_data[$key] = $value;
@@ -48,14 +52,27 @@ class GlobalStateDB extends Component implements GlobalStateInterface
         }
     }
 
+    private function getValue($name)
+    {
+        if (is_string($this->_data[$name])) {
+            try {
+                $value = unserialize($this->_data[$name]);
+                if ($value !== false) {
+                    $this->_data[$name] = $value;
+                }
+            } catch (\Exception $e) {
+
+            }
+        }
+
+        return $this->_data[$name];
+    }
+
     private function write($data)
     {
+        $sql = 'REPLACE INTO '.$this->tableName.' (`'.$this->fieldName.'`, `'.$this->fieldValue.'`) VALUES (:name, :value)';
         foreach ($data as $name => $value) {
-            \Yii::$app->db->createCommand()->update(
-                $this->tableName,
-                [$this->fieldValue => $value],
-                [$this->fieldName => $name]
-            )->execute();
+            \Yii::$app->db->createCommand($sql, [':name' => $name, ':value' => $value])->execute();
         }
     }
 }
